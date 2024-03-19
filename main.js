aa97671d96e2d05323c48255d5af6e4a62e7937e
Qhaleed <reslykadiri@gmail.com>
2024-03-19 22:30:11 +0800

    test

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ window.onscroll = function (){
 }
 
 function scrollFuncntion () {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
         navbar.style.backgroundColor = "rgb(19, 24, 63)";
         navbar.style.transition = "500ms";
     }
