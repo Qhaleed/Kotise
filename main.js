37aec4823bffd89ac259a838eea869cdb83b4c79
Qhaleed <reslykadiri@gmail.com>
2024-03-20 03:46:06 +0800

    final na promise

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 
-   
-    var submit = document.getElementById("submit");
-    var username = document.getElementById("name").value;
-    var user = document.getElementById("user");
-    var pass = document.getElementById("pass");
-    var confirmPass = document.getElementById("conPass");
-    var mobileNumber = document.getElementById("mobileNumber").value;
-    var emails = document.getElementById("emails").value;
-    var signup = document.getElementById('signup');
+var submit = document.getElementById("submit");
 
-
-    submit.addEventListener('click', validation)
+submit.addEventListener('click', validation)
 
 function validation() {
-  if (pass.value != "" && user.value != "" ) {
-    alert("Registration successful");
-    window.location.href = "components/LoginForm/main/index.html";
+  var signup = document.getElementById('signup');
+ 
+  var username = document.getElementById("name").value;
+  var user = document.getElementById("user");
+  var pass = document.getElementById("pass");
+  var confirmPass = document.getElementById("conPass");
+  var mobileNumber = document.getElementById("mobileNumber").value;
+  var emails = document.getElementById("emails").value;
+ 
+
+  if (pass.value !== "" && user.value !== "" && username !== "" && confirmPass.value === pass.value) {
+    signup.textContent = "Registration Successful...";
+    setTimeout(() => {
+      window.location.href = "components/LoginForm/main/index.html";
+    }, 500);
+    
   } else {
     alert("Registration Unsuccessful");
+    signup.textContent = "Try again";
     
   }
  
